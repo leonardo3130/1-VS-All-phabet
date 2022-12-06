2c6076be75d295a3a11fa1393e314727d6c6e46b
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2022-12-06 12:18:02 +0100

    fixato bug della classe Player

--- Player.cpp
+++ Player.cpp
@@ -1,14 +1,20 @@
 #include "Player.hpp"
+#include <cstring>
 
 //costruttore
-Player::Player(char nick[], int hp = 10, int def = 2, int atk = 2) {
-    strcpy(this.nick, nick);
+Player::Player(char nick[], char look = 'S', int hp = 10, int def = 2, int atk = 2) {
+    strcpy(this->nick, nick);
+    this->look = look;
     this->hp = hp;
     this->def = def;
     this->atk = atk;
 };
 
 
+void Player::setHp(int new_hp){
+    this->hp = new_hp;
+}
 
-void Player::setHp(int new_hp);
-int Player::getHp(){return this.hp;}
+int Player::getHp(){
+    return this->hp;
+};
