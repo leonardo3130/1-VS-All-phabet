272e631e3f158a919bf7730c3d72b0a46c78085e
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2022-12-06 12:18:02 +0100

    fixato bug della classe Player

--- Player.hpp
+++ Player.hpp
@@ -1,12 +1,16 @@
 class Player{
     protected:
         char nick[20];  //nome del player
+        char look;      //lettera usata per identificare il personaggio 
         int hp;         //numero totale di hp
         int def;        //valore difesa
         int atk;        //valore attacco
+        int x;          //coordinata x
+        int y;          //coordinata y
 
     public:
-        Player(char nick[], int hp = 10, int def = 2, int atk = 2);
+        // costruttore
+        Player(char nick[], char look = 'S', int hp = 10, int def = 2, int atk = 2);
 
         //mancano metodi getter e setter
         void setHp(int new_hp);
