2722bc7eeb21ffe87a5ea92b8a71adceeae94eee
LucaSlv3 <119587536+LucaSlv3@users.noreply.github.com>
2022-12-11 19:33:43 +0100

    Update character.hpp

--- character.hpp
+++ character.hpp
@@ -6,6 +6,7 @@ class Character{
     int hp;     //numero totale di hp
     int atk;    //valore attacco (contatto)
     bool gun;   //il personaggio spara?
+                //(no valore di difesa perchè ai mostri non serve: non comprano aramture)
   
   public:
     charater(int x = 0, int y = 0, char look = "/", int hp = 1, int atk = 1, bool gun = false);      //costruttore
