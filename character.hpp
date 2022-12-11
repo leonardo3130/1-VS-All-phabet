a1ad693fd960f06046cc573a492ec1724d464716
LucaSlv3 <119587536+LucaSlv3@users.noreply.github.com>
2022-12-11 19:30:30 +0100

    Update character.hpp

--- character.hpp
+++ character.hpp
@@ -1,1 +1,12 @@
-
+class Character{
+  protected:
+    int x;      //coordinata x
+    int y;      //coordinata y
+    char look;  //lettera usata per identificare il personaggio
+    int hp;     //numero totale di hp
+    int atk;    //valore attacco (contatto)
+    bool gun;   //il personaggio spara?
+  
+  public:
+    charater(int x = 0, int y = 0, char look = "/", int hp = 1, int atk = 1, bool gun = false);      //costruttore
+};
