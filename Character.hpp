cf164b733367e46a249011141d390d6d5afbc5a3
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2022-12-29 18:22:58 +0100

    Fix Bug in Character

--- Character.hpp
+++ Character.hpp
@@ -1,4 +1,4 @@
-class character{
+class Character{
   protected:
     int x;      //coordinata x
     int y;      //coordinata y
@@ -7,10 +7,11 @@ class character{
     int atk;    //valore attacco (contatto)
     bool gun;   //il personaggio spara?
                 //(no valore di difesa perchè ai mostri non serve: non comprano aramture)
-  
+
   public:
     //costruttore
-    character(int x = 0, int y = 0, char look = '/', int hp = 1, int atk = 1, bool gun = false);      
-    
+    Character(int x = 0, int y = 0, char look = '/', int hp = 1, int atk = 1, bool gun = false);
+
     //bool xy: true->asse x, false->asse y; int val: celle in una mossa
-    void move(bool xy, int val = 1);  };
+    void move(int new_x, int new_y=0, int mode = 0);
+};
