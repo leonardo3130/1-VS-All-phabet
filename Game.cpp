86f43a9e545a9af05e6dd5d40b852a0447f64f7e
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2023-04-13 09:37:50 +0200

    git pull origin main ;

--- Game.cpp
+++ Game.cpp
@@ -5,6 +5,9 @@
 #include "Character.hpp"
 #include "Player.hpp"
 #include "Game.hpp"
+#include <unistd.h>
+
+#define NUM_COLORS 8
 
 Game::Game() {};
 
@@ -13,7 +16,7 @@ void Game::run() {
 	Character protagonist(3,3,10,10,10,0,'1');
 	Map map(40,80);
 
-	initscr(); cbreak(); noecho();keypad(stdscr, TRUE);
+	//initscr(); cbreak(); noecho();keypad(stdscr, TRUE);
 	nodelay(stdscr, TRUE);
 	WINDOW *game_win;
 
@@ -25,7 +28,7 @@ void Game::run() {
 	curs_set(0);
 	refresh();
 	for(int i = 0 ; i < map.getHeight() ; i++)
-	{    
+	{
 		for(int j = 0 ; j < map.getWidth() ; j++)
 		{
 			mvwprintw(game_win, i, j, "%c",map.getMapChar(i, j));
@@ -39,7 +42,7 @@ void Game::run() {
 	time_t previous_time = time(nullptr);
 	time_t lag = time_t(0.0);
 	time_t current, elapsed;
-	int prev_x, prev_y; 
+	int prev_x, prev_y;
 	int const MS = 100;
 	while(true) //condizione che andrà in base ad hp e altro
 	{
@@ -61,8 +64,8 @@ void Game::run() {
 		draw(game_win, map, protagonist, prev_x, prev_y);
 		//shooting
 		//mostri
-	}        
-	getch();                                                                                                                                            
+	}
+	getch();
     endwin();
 }
 
@@ -80,7 +83,7 @@ void Game::handleInput(int c, Map& map, Character& protagonist) {
 			case KEY_LEFT:
 				protagonist.moveleft(map);
 				break;
-			case KEY_RIGHT: 
+			case KEY_RIGHT:
 				protagonist.moveright(map);
 				break;
 			default:
@@ -99,4 +102,128 @@ void Game::draw(WINDOW* win, Map& map, Character& protagonist, int prev_x, int p
 	wrefresh(win);
 	mvwprintw(win, protagonist.getY(), protagonist.getX(), "%c",protagonist.getLook());
 	wrefresh(win);
+}
+
+void Game::timed_print(char *text, int text_len, int micro_seconds_delay, int l, int c){
+    // Impostazione dei colori
+    start_color();
+
+    for (int i = 1; i <= NUM_COLORS; i++) {
+        init_pair(i, i, COLOR_BLACK);
+    }
+
+    // Impostazione dei colori per la scritta
+    attron(COLOR_PAIR(1));
+
+    for (int i = 0; i < text_len - 1; i++) {
+        attron(COLOR_PAIR(rand() % NUM_COLORS + 1)); // Imposta un colore casuale
+        mvprintw(l, c + i, "%c", text[i]);
+        refresh();
+        usleep(micro_seconds_delay); // Ritardo di 100 millisecondi
+    }
+
+    for (int i = 1; i <= NUM_COLORS; i++) {
+        attroff(COLOR_PAIR(i));
+    }
+}
+
+//messaggio iniziale
+void Game::init_message(){
+    // Animazioni
+    char text[] = "Welcome in 1 vs (All)phabet.";
+    timed_print(text, 29, 100000, LINES/3, COLS/2 - 14);
+
+    strcpy(text, "Press any key...");
+    timed_print(text, 17, 30000, LINES/3 + 1, COLS/2 - 8);
+
+    // Attende la pressione di un tasto per chiudere la finestra
+    getch();
+}
+
+//get credentials from the terminal
+void Game::getCredentials(char* username, char* password) {
+    // Crea il box verde
+    attron(COLOR_PAIR(COLOR_GREEN));
+    box(stdscr, 0, 0);
+
+    mvprintw(LINES/2 - 4, COLS/2 - 14, "Please enter your credentials:");
+    mvprintw(LINES/2 - 2, COLS/2 - 15, "Username: ");
+    mvprintw(LINES/2, COLS/2 - 15, "Password: ");
+
+    // Crea la finestra di input per l'username
+    mvprintw(LINES/2 - 2, COLS/2 - 7, "[               ]");
+
+    // Crea la finestra di input per la password
+    mvprintw(LINES/2, COLS/2 - 7, "[               ]");
+    attroff(COLOR_PAIR(COLOR_GREEN));
+
+    // Sposta il cursore sulla finestra dell'username
+    move(LINES/2 - 2, COLS/2 - 4);
+    echo();
+    getstr(username);
+
+    // Sposta il cursore sulla finestra della password
+    move(LINES/2, COLS/2 - 4);
+    noecho();
+    getstr(password);
+
+    // Pulisco lo schermo
+    clear();
+}
+
+//menu per scegliere se effettuare login, sign in o uscire dal gioco
+int Game::choice_menu() {
+    int choice = 0;
+    string choices[3]={"Login", "Sign in", "Exit"};
+    int highlights = 0;
+
+    WINDOW *menu = newwin(6, 12 , LINES/2 -1, COLS/2 -2);
+    box(menu, 0, 0);
+    refresh();
+    wrefresh(menu);
+
+    keypad(menu, true);
+
+    while (choice != 10){
+        for(int i=0; i< sizeof(choices); i++){
+            if(i == highlights)
+                wattron(menu, A_REVERSE);
+            mvwprintw(menu, i+1, 1, choices[i].c_str());
+            wattroff(menu, A_REVERSE);
+        }
+        choice = wgetch(menu);
+
+        switch (choice)
+        {
+        case KEY_UP:
+            highlights--;
+            if(highlights == -1)
+                highlights = 0;
+            break;
+        case KEY_DOWN:
+            highlights++;
+            if(highlights == 3)
+                highlights = 2;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return highlights;
+}
+
+void Game::game_exit(){
+    char text[11];
+    strcpy(text, "Goodbye...");
+    timed_print(text, 11, 50000, LINES/3, COLS/2 - 5);
+
+    /*
+        code for the backup
+    */
+
+    getch();
+    endwin();
+    exit(0);
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-//gcc gioco.cpp -lncurses -o gioco
-
-//#include <ncurses.h>
+#include <ncurses.h>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <unistd.h>
 
 #include "Map.hpp"
 #include "Player.hpp"
 #include "Game.hpp"
 
+
+#define NUM_COLORS 8
 using namespace std;
 
 typedef struct livelli{
@@ -24,23 +27,22 @@ struct Session{
 };
 
 //funzione per creare un nuovo account
-bool signIn(char *psw, char character, char *filename){
+bool signIn(char *psw, char *filename){
 	bool isCorrect = false;
 
 	//scrivo nel file delle credenziali le informazioni degli utenti
 	ofstream credenziali;
 	credenziali.open(filename);
 	credenziali << psw << endl;
-	credenziali << character << endl;
 
-	Character c = Character(character);
+	Character c = Character('1');
 	credenziali << c.getHp() << endl;
 	credenziali << c.getAtk() << endl;
 	credenziali << c.getDef() << endl;
 	credenziali << 100 << endl;		//default money
-
 	credenziali.close();
 	isCorrect = true;
+
 	return isCorrect;
 }
 
@@ -48,9 +50,8 @@ bool signIn(char *psw, char character, char *filename){
 bool login(char *user, char *psw, char *filename){
 	ifstream input_file(filename);
 	int hp = 0, atk = 0, def = 0, money = 0;
-	char ch;
-
 	bool exists;
+
     if (input_file.is_open()) {
 		//leggo la psw
         char line[50];
@@ -58,95 +59,80 @@ bool login(char *user, char *psw, char *filename){
 
 		//se la psw è giusta prendo le informazioni del player
 		if(strcmp(line, psw) == 0)	{
-
-			input_file >> ch;
 			input_file >> hp;
 			input_file >> atk;
 			input_file >> def;
 			input_file >> money;
 			exists = true;
 
-			Character c = Character(100, 100, ch, hp, atk, 0, def);
-			Player p = Player(user, psw, money, c);
+			//Character c = Character(100, 100, '1', hp, atk, def);
+			//Player p = Player(user, psw, money, c);
 		}else {
-			cout << "Password errata.\n";
+			//Password errata
+            attron(COLOR_PAIR(COLOR_RED));
+            mvprintw(LINES/2 + 3, COLS/2 - 14, "Wrong password!");
+            attroff(COLOR_PAIR(COLOR_RED));
 			exists = false;
 		}
 
     } else {
         // Il file non esiste
+        attron(COLOR_PAIR(COLOR_RED));
+        mvprintw(LINES/2 + 3, COLS/2 - 14, "Wrong username!");
+        attroff(COLOR_PAIR(COLOR_RED));
 		exists = false;
-        cout << "Giocatore non esistente.\n";
     }
 	return exists;
 }
 
+
 int main(){
-	//inizio Login o Creazione account
-	bool correct_input = false;
-	char scelta = ' ';
+	srand(time(NULL));
 
-	char username[20] = "Tizio";	//nome default
+	// Inizializzazione della libreria ncurses
+	initscr();cbreak();noecho();
 
-	char psw[20];
-	char character;
-
-	cout << "Benvenuto nel gioco premi: \n  -\"L/l\" per loggarti \n  -\"C/c\" per creare un account \n  -\"E/e\" per uscire \nScelta: ";
-	cin >> scelta;
-	do{
-		char filename[31] = "Archivio/";
-		if(scelta ==  'L' || scelta == 'l'){
-			//login
-			cout << "Username: ";
-			cin >> username;
-			strcat(filename, username);
-			strcat(filename, "/credentials.txt");
-
-			cout << filename <<" Password: ";
-			cin >> psw;
-
-			correct_input = login(username, psw, filename);
-		}else if(scelta == 'C' || scelta == 'c'){
-			//crea account
-			cout << "Username: ";
-			cin >> username;
-			char mkdir[50] = "mkdir Archivio/";
-			system(strcat(mkdir, username));
-			strcat(filename, username);
-			strcat(filename, "/credentials.txt");
-
-			cout << filename <<" Password: ";
-			cin >> psw;
-
-			cout << "Carattere da usare in game: ";
-			cin >> character;
-
-			correct_input = signIn(psw, character, filename);
-
-		}else if(scelta == 'E' || scelta == 'e'){
-			//termina partita
-			exit(0);
-		}else {
-			bool ctrl = false;
-			while(scelta != 'E' && scelta != 'e' &&	 scelta != 'L' && scelta != 'l' && scelta != 'C' && scelta != 'c'){
-				cout << "Carattere errato, ";
-				cout << "premi: \n  -\"L/l\" per loggarti \n  -\"C/c\" per creare un account \n  -\"E/e\" per uscire \nScelta: ";
-				cin >> scelta;
-			}
-		}
-
-	}while(!correct_input);
-	cout << "--Inizio partita (da fare)\n" ;
-	//INIZIALIZZAZIONE LIVELLI +
-	/*
-	initscr();			        // Start curses mode
-	printw("Hello World !!!");	// Print Hello World
-	refresh();			        // Print it on to the real screen
-	getch();			        // Wait for user input
-	endwin();			        // End curses mode
-
-	*/
+	//inizio game + messaggio iniziale (grafica)
 	Game game = Game();
-	game.run();
+	game.init_message();
+	clear();
+
+	//vedo se il giocatore desidera effetuare il login (0), registrarsi (1) o uscire (2)
+	int choice = game.choice_menu();
+	clear();
+
+	char username[20], password[20], filename[50], mkdir[50];
+	strcpy(mkdir, "mkdir Archivio/");
+	bool correct_input = false;
+
+	//scrivo in filename la path per raggiungere le credenziali
+	while(!correct_input){
+		if(choice <= 1){
+			game.getCredentials(username, password);
+			strcpy(filename, "Archivio/");
+			strcat(filename, username);
+			strcat(filename, "/credentials.txt");
+		}
+		switch(choice){
+			case 0:
+                correct_input = login(username, password, filename);
+                break;
+            case 1:
+                //creo la cartella per salvare i file di gioco del giocatore (rinominata col suo nome)
+                system(strcat(mkdir, username));
+                correct_input = signIn(password, filename);
+                break;
+            case 2:
+                game.game_exit();
+                break;
+            default:
+                break;
+
+        }
+	}
+	
+	if(choice <= 1){
+		game.run();
+	}
 	return 0;
 }
