346e7c9713f1ca80a5d786dbe66bff63ecea54e2
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2023-04-13 09:37:50 +0200

    git pull origin main ;

--- Game.hpp
+++ Game.hpp
@@ -3,18 +3,23 @@
 #include "Character.hpp"
 #include "Player.hpp"
 #include <ncurses.h>
-
+#pragma once
 
 using namespace std;
 
 class Game {
 	private:
-		//WINDOW *create_new_win(int h, int w, int y, int x); 
+		//WINDOW *create_new_win(int h, int w, int y, int x);
 		void handleInput(int c, Map& map, Character& protagonist);
 		void draw(WINDOW* win, Map& map, Character& protagonist, int prev_x, int prev_y);
 		void update(Map& map, Character& protagonist, int prev_x, int prev_y);
 	public:
 		Game();
 		void run();
+		void timed_print(char *text, int text_len, int micro_seconds_delay, int l, int c);
+		void init_message();
+		void getCredentials(char* username, char* password);
+		int choice_menu();
+		void game_exit();
 		//bool isOver();
 };
