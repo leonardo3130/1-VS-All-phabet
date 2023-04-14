885558578233fb8744d600fccd84d25bf7e0e028
Lucas <silvestri.luca@outlook.com>
2023-04-14 02:25:53 +0200

    modificato metodo shot e aggiunto metodo bullet_check, risolti errori

--- Bullet.hpp
+++ Bullet.hpp
@@ -1,24 +1,19 @@
-#include <iostream>
-#include <ctime>
-
 #include "Map.hpp"
-#include "Player.hpp"
-
-
-using namespace std;
+#include <ncurses.h>
 
 class Bullet{
     protected:
         double speed;   //velocità: 1 corrisponde al movimento di una cella al secondo, la formula dell'intervallo di movimento è (1 sec) / (speed)
-        int x;          //posizione di partenza del poriettile
-        int y;          //
-        int dir;        //direzioen proiettile (corriponde alla mode del personaggio da cui viene sparato)
+        
         char look;
 
     public:
+        int x;          //posizione di partenza del poriettile
+        int y;          //
+        int dir;        //direzioen proiettile (corriponde alla mode del personaggio da cui viene sparato)
         int id; ///codice identificativo univoco per ogni istanza
-        Bullet(double speed = 1, int x = 0, int y = 0, int dir = 0,  int id = 0, char look = '.');
-        void shot(Map mappa, Player p); 
+        Bullet(double speed = 1, int x = 0, int y = 0, int dir = 0,  int *id = 0, char look = '.');
+        void shot(Map mappa); 
 };
 
 //codice per lista proiettili
@@ -59,6 +54,65 @@ pbul delete_bullet(pbul p, int val_id){
 		}
 		return(head);
 	}
+}
+pbul search_bullet_by_xy(pbul lista_proiettili, int x, int y){
+        pbul tmp = lista_proiettili;
+        bool found = false;
+        while(!found && tmp != NULL){
+                if(tmp->bul.x == x && tmp->bul.y == y){
+                        found = true;
+                }
+                else
+                        tmp = tmp->next;
+        }
+        return tmp;
+}
+
+
+
+
+//Bullet.cpp
+
+#include <ncurses.h>
+//#include "Bullet.hpp"
+
+Bullet::Bullet(double speed, int x, int y, int dir, int *id, char look){
+    this->speed = speed; //velocita' del proiettile
+    this->x = x; //posizione di partenza del proiettile
+    this->y = y;
+    if(dir>=0 && dir<=3){ this->dir = dir; } else{ this->dir = 0; }
+    this->look = look; 
+    this->id = *id; //codice identificativo univoco per ogni istanza
+}
+
+void Bullet::shot(Map mappa){ 
+
+    int bul_delay = 100 / this->speed; //100 millisecondi / velocità : più è alta la velocità, minore è il delay di movimento del proiettile
+
+    if(this->dir == 0){
+        while(mappa.isempty(this->x + 1, this->y) == true){ 
+            napms(bul_delay);
+            this->x += 1; //proiettile sparato a destra
+        }
+    }
+    else if(this->dir == 2){
+        while(mappa.isempty(this->x - 1, this->y) == true){
+            napms(bul_delay);
+            this->x -= 1; //proiettile sparato a sinistra
+        }
+    }
+    else if(this->dir == 3){
+        while(mappa.isempty(this->x, this->y + 1) == true){
+            napms(bul_delay);
+            this->y += 1; //proiettile sparato in alto
+        }
+    }
+    else if(this->dir == 1){
+        while(mappa.isempty(this->x, this->y - 1) == true){
+            napms(bul_delay);
+            this->y -= 1; //proiettile sparato in basso
+        }
+    }
 }
 
 
--- Player.cpp
+++ Player.cpp
@@ -1,12 +1,7 @@
-#include <iostream>
-#include "Player.hpp"
+//#include "Player.hpp"
 
-Player::Player(char* nick, char* psw, int m, Character pers):Character(x, y, mode, hp, atk, def, look){
+Player::Player(char* nick, char* psw, int m):Character(x, y, mode, hp, atk, def, look){
     strcpy(this->nick, nick);
     strcpy(this->psw, psw); 
     this->monete = m;
 };
-
-
-
-
