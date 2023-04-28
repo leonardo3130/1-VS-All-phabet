46842be93471df5b06ce695eec64de731b635d58
leonardo3130 <leonardopo3130@gmail.com>
2023-04-28 17:12:45 +0200

    il player raccoglie le monete

--- Player.hpp
+++ Player.hpp
@@ -8,9 +8,10 @@ class Player : public Character{
     protected:
         char nick[20];  //nome del player
         char psw[20];   //psw del player
-        int money;     //numero di monete del giocatore
+        
 
     public:
+        int money;     //numero di monete del giocatore
         Player(char* nick, char* psw, int monete, char l);
         char* getNick();
         int getMoney();
