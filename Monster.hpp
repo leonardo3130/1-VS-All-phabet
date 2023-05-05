6491ce56e78bae750762d68ea94aa9319b0f03e5
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2023-05-05 15:14:07 +0200

    continuo bilista + risoluzione bug su mostri che prendono le moneteù

--- Monster.hpp
+++ Monster.hpp
@@ -15,6 +15,10 @@ class Monster : public Character{
         int shot_fr; //ogni quanto spara
         Monster(int x = 2, int y = 3,int hp = 100, int atk = 10,int def = 10, int mode = 0, char look = 'A', int id = 0, int speed = 1, int shot_fr = 1);
         void move(Map& mappa, int x_p, int y_p);
+        void moveright(Map mappa);
+        void moveleft(Map mappa);
+        void moveup(Map mappa);
+        void movedown(Map mappa);
 };
 
 
@@ -33,3 +37,5 @@ pmon new_monster(pmon lista, Monster m);
 pmon delete_monster(pmon p, int val_id);
 
 pmon search_monster_by_xy(pmon lista_mostri, int x, int y);
+
+
