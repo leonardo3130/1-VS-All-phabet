278c349a19fb93eb940fd547b77c42ced13f2bd1
Andrea Fiorellino <andrea.fiorelino03@gmail.com>
2023-05-06 21:07:49 +0200

    Aggiunta struct di sessione e spostamentto dei metodi getCredentials, choiceMenu, login, SignIn in player + aggiunta costruttore nullo + modifica prototipo dei metodi run e draw stats

--- Player.hpp
+++ Player.hpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include "Character.hpp"
 #include "Monster.hpp"
+using namespace std;
 
 struct arnd{
     int right;
@@ -15,16 +16,19 @@ class Player : public Character{
     protected:
         char nick[20];  //nome del player
         char psw[20];   //psw del player
-        
 
     public:
         int money;     //numero di monete del giocatore
+        Player();
         Player(char* nick, char* psw, int monete, char l);
         int fight(int m_hp, int m_atk, int m_def);                //interazione tra mostro e player
         char* getNick();
         int getMoney();
         void takeMoney(int value);
         arnd check_around(Map& m);  //ritorna una struct con 4 interi che dicono cosa si trova nelle 4 posizioni intorno al player: 0 vuoto, 1 mostro, 2 moneta
+        int choice_menu();
+        bool signIn(char *psw, char *filename);
+        bool login(char *user, char *psw, char *filename);
 };
 
 
