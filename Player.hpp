f00792944cfdbf902f85315b38c45a4476662aa0
leonardo3130 <leonardopo313@gmail.com>
2023-05-13 19:46:56 +0200

    Se si vuole testare il gioco non effettuare il login, va solo col sign in. L'errore si trova nel costruttore di game nel caso debba caricare la mappa da file

--- Player.hpp
+++ Player.hpp
@@ -24,6 +24,7 @@ class Player : public Character{
         int fight(int m_hp, int m_atk, int m_def);                //interazione tra mostro e player
         char* getNick();
         int getMoney();
+        void pay(int p);
         void takeMoney(int value);
         arnd check_around(Map &m);  //ritorna una struct con 4 interi che dicono cosa si trova nelle 4 posizioni intorno al player: 0 vuoto, 1 mostro, 2 moneta
         int choice_menu();
