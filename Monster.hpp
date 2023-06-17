dbe5794a571e2802dbd12ca83fae61db51f3e57b
Lucas <silvestri.luca@outlook.com>
2023-06-17 15:22:04 +0200

    rimossi metodi inutili da monster

--- Monster.hpp
+++ Monster.hpp
@@ -10,16 +10,10 @@ class Monster : public Character{
         bool turret;
 
     public:
-        //int id; //codice univoco per ogni istanza
         Monster(int x = 2, int y = 3,int hp = 100, int atk = 10,int def = 10, int mode = 0, char look = 'A', bool tur = 0);
         void move(Map& mappa, int x_p, int y_p);
-        void moveright(Map mappa);
-        void moveleft(Map mappa);
-        void moveup(Map mappa);
-        void movedown(Map mappa);
         bool getTur();
 };
-
 
 //codice per lista mostri
 typedef struct mlist{
@@ -28,7 +22,6 @@ typedef struct mlist{
     int prev_y;
     mlist *next;
 }* pmon;
-
 
 //insert
 pmon new_monster(pmon lista, Monster m);
